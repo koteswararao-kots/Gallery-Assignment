f942d1e7c4248f5438391f6b239f568ea4ef4fc5
koteswararao Madduru <kotsmaddhur@gamil.com>
2022-02-06 21:03:22 +0530

    finalChanges

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get('https://reqres.in/api/users');
+      setUsers(response.data.data);
+    }
+    loadUsers();
+  }, []);
+
+  const onSuggestHandler = (text) => {
+    setText(text);
+    setSuggestions([]);
+  }
+
+  const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi");
+        return user.email.match(regex)
+
+      })
+    }
+    console.log('matches', matches)
+    setSuggestions(matches)
+    setText(text)
+  }
+
+
+
   useEffect(() => {
     fetchItems();
   }, []);
@@ -17,10 +52,18 @@ function App() {
   };
 
   return (
+
+
     <div className="container">
       <div className='header'>
-        <div><input type="text" /></div>
-        <div><h5>search</h5></div>
+        <input className='input-bar' type="text" onChange={e => onChangeHandler(e.target.value)}
+          value={text} />
+        <h5>search</h5>
+
+        {suggestions && suggestions.map((suggestion, i) =>
+          <div className='suggestion' onClick={() => onSuggestHandler(suggestion.email)}>
+            {suggestion.email}</div>
+        )}
 
 
       </div>
@@ -28,10 +71,10 @@ function App() {
       <div className="row-images">
         {items.map((post) =>
           <div >
-          <Link to= '/staticPage'> <img src={post.image} alt = "images" /></Link>
-          <p>{post.title}</p>
-          <br />
-          <p>{post.description}</p>
+            <Link to='/staticPage'> <img src={post.image} alt="images" /></Link>
+            <p>{post.title}</p>
+            <br />
+            <p>{post.description}</p>
 
           </div>
         )}
